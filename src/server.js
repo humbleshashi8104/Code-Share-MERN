0634903d0f3ecf6e3e5f3591852f5800b1d57d01
Shashi Chaubey <kumarshashi8104@gmail.com>
2024-08-31 01:31:54 +0530

    Middleware routing setup done in server.js

--- src/server.js
+++ src/server.js
@@ -9,6 +9,10 @@ app.use(cors())
 connectDb()
 const port = process.env.PORT || 5000
 
+app.use(errorHandler);
+app.use(express.json())
+app.use('/', require('./routes/pasteroutes'))
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
